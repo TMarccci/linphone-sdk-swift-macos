36d92284e78a211a79164ad8cc50e6ff90318a9e
Tihanyi Marcell <marcitihanyi@gmail.com>
2025-09-19 22:53:57 +0200

    Rename

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "linphonesw",
+            name: "linphonesw-macos",
             targets: ["linphonesw"]
         )
     ],
